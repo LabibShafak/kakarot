b143cbf368e6f52ebad035f8eff7d89f97b7f06f
LabibShafak <161319628+LabibShafak@users.noreply.github.com>
2024-02-26 18:08:21 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+const str = ' by turning my anger into power i have gained yet another new power';
+const words = str.split(' ');
+console.log(words[8]);
+const chars = str.split('');
+console.log(chars.length);
+
